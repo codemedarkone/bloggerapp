c8fa89c12b78769287758b4a0c3b152e9a1a3591
Chris <codemedarkone@gmail.com>
2017-05-31 17:22:53 -0600

    created notice and actions in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,52 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+  @article = Article.new(article_params)
+
+  respond_to do |format|
+    if @article.save
+      format.html { redirect_to article_path(@article), notice: "Successfully created article" }
+    else
+      format.html { :new }
+    end
+  end  
+end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    @article.destroy
+    respond_to do |format|     
+      format.html { redirect_to :articles, notice: "Article was successfully destroyed!" }
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to article_path, notice: "Article has been updated" }
+      else
+        format.html { :new }
+      end
+    end
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
